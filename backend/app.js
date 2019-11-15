81ffd50aad4463f06b8036d16af3b7bb2ac07f99
jpenson <jack.penson@anaplan.com>
2019-11-15 22:10:35 +0000

    payment hook api

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,11 @@ const port = process.env.PORT || 7077;
 require('dotenv').config();
 
 const message = require('./server/routes/messages.js');
+const payments = require('./server/routes/payments.js');
 
 app.use(bodyParser({ limit: '500mb' }));
-app.use('/api', message);
+app.use('/api/notifications', message);
+app.use('/api/payments', payments);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
---
+++ backend/server/routes/payments.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { Expo } = require('expo-server-sdk');
+
+const expo = new Expo();
+const router = express.Router();
+
+router.use(express.json());
+
+router.get('/transaction', (req, res) => {
+  console.log(JSON.stringify(req.body));
+  return res.json({ ok: true });
+});
+
+module.exports = router;
