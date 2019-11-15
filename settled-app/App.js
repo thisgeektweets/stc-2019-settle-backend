188b64bd7646991a5b5c7776ea681bbc1f31e8a2
jpenson <jack.penson@anaplan.com>
2019-11-15 21:19:25 +0000

    register device

--- settled-app/App.js
+++ settled-app/App.js
@@ -2,7 +2,9 @@ import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import {
+  Platform, StatusBar, StyleSheet, View,
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import AppNavigator from './navigation/AppNavigator';
@@ -18,22 +20,21 @@ export default function App(props) {
         onFinish={() => handleFinishLoading(setLoadingComplete)}
       />
     );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
   }
+  return (
+    <View style={styles.container}>
+      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+      <AppNavigator />
+    </View>
+  );
 }
 
 async function loadResourcesAsync() {
   await Promise.all([
     Asset.loadAsync([
       require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'), 
-      require('./screens/figbar.png'), 
+      require('./assets/images/robot-prod.png'),
+      require('./screens/figbar.png'),
     ]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
